794c33d2834b3eeabd4cf8d1b333c7deaae20cd3
VictorOrduz <victicordux@gmail.com>
2025-08-09 22:18:11 -0500

    archivo subido a github

---
+++ adivinaElNumero.py
@@ -0,0 +1,18 @@
+#Adivina el numero
+
+"""
+crea un juego en el que la computadora elige un numero al azar entre 1 y 100 y el usuario tiene que adivinarlo, el programa debe dar pistas al usuario si su numero es mayor o menor que el numero secreto. El juego termina cuando el usuario adivina el numero
+1. importa la libreria random
+2. genera un numero aleatorio entre 1 y 100 y guardalo en una variable
+3. inicia un bucle que se repita hasta que el usuario adivine el numero
+4. dentro del bucle pide al usuario que ingrese un numero
+5. compara el numero del usuario con el numero secreto y da una pista ('demasiado alto', 'demasiado bajo')
+6. si el usuario adivina, imprime un mensaje de felicitacion y termina el bucle
+"""
+from random import randint
+
+numero_secreto = randint
+
+numero = int(input("ingrese un numero: "))
+
+contador = 1
